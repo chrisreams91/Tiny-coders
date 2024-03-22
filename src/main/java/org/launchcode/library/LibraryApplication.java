45b396376e62404a21f83b4d68933848706ac6c9
megha saxena <meghasaxena9@gmail.com>
2024-03-21 20:30:33 -0500

    message

--- src/main/java/org/launchcode/library/LibraryApplication.java
+++ src/main/java/org/launchcode/library/LibraryApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
+	hi;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
