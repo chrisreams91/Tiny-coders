d6572de8751135b93a690269cd9cec5d5af00ba5
megha saxena <meghasaxena9@gmail.com>
2024-03-24 22:11:24 -0500

    megha 3/24

--- src/main/java/org/launchcode/library/LibraryApplication.java
+++ src/main/java/org/launchcode/library/LibraryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
-	hi;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
---
+++ src/main/java/org/launchcode/library/controllers/StudentController.java
@@ -0,0 +1,57 @@
+package org.launchcode.library.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.library.data.StudentRepository;
+import org.launchcode.library.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping ("students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("create")
+    public String renderCreateStudentForm(Model model){
+        model.addAttribute("title", "Create Student");
+        model.addAttribute(new Student());
+        model.addAttribute("categories", studentRepository.findAll());
+        return "student/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@ModelAttribute @Valid Student newEvent, Errors errors, Model model){
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Student");
+            return "students/create";
+        }
+        studentRepository.save(newEvent);
+        return "redirect:/students";
+
+    }
+
+    @GetMapping ("delete")
+    public String displayDeleteStudentForm (Model model){
+        model.addAttribute("title", "Delete Student");
+        model.addAttribute("events", studentRepository.findAll());
+        return "student/delete";
+    }
+
+    @PostMapping ("delete")
+    public String processDeleteEvent(@RequestParam(required = false) int[] studentIds){
+        if (studentIds != null)
+        {
+            for (int id : studentIds) {
+                studentRepository.deleteById(id);
+            }
+        }
+        return "redirect:/students";
+
+    }
+}
